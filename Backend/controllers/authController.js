c66dc7723921a54ef5fe9bfd2cb7447fe7c2815c
Abhishek <srivastavaabhishek.8055@gmail.com>
2026-08-09 15:06:08 +0530

    Add authorization controller

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,100 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const userRegister = async (req, res) => {
+
+    try{
+        const { username, email, password } = req.body
+
+        if(!username || !email || !password){
+            return res.status(400).json({ message : "All Fields are mandatory!" })
+        }
+
+        const existingUser = await User.findOne({ 
+            $or : [{ username }, { email }]
+        })
+
+        if(existingUser){
+            return res.status(409).json({
+                message : "User already Registered"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            username,
+            email,
+            password : hashedPassword
+        })
+
+        const token = jwt.sign({id : user._id, username : user.username}, process.env.JWT_SECRET_TOKEN, {expiresIn : "1d"})
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            message : `User registed with username ${user.username}`,
+            user : {
+                id : user._id,
+                username : user.username,
+                email : user.email
+            }
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message : `Something wen't wrong`})
+    }
+}
+
+const userLogin = async (req, res) =>{
+    try{
+        const { username, password } = req.body
+
+        if(!username || !password){
+            return res.status(400).json({ message : "All Fields are mandatory!"})
+        }
+
+        const user = await User.findOne({ username })
+
+        if(!user){
+            return res.status(404).json({
+                message : "User not found!"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            res.status(401).json({message : "Invalid username or password"})
+        }
+
+        const token = jwt.sign({id : user._id, username : user.username}, process.env.JWT_SECRET_TOKEN, {expiresIn : "1d"})
+
+        res.cookie("token", token)
+
+        res.status(200).json({ 
+            message : "User logged in successfully.",
+            user : {
+                id : user._id,
+                username : user.username,
+                email : user.email
+            }
+         })
+    }catch(error){
+        console.log(error)
+        res.status(501).json({message : "Something wen't wrong, retry after some time"})
+    }
+}
+
+const userLogout = async(req, res) => {
+    const token = req.cookies.token
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message : "User logged out successfully."
+    })
+}
+
+module.exports = { userRegister, userLogin, userLogout}
